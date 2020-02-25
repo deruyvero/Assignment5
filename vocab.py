f1802c4fb031fdd9b7a56310499aafd8f67fdcba
U058716 <olivier.deruyver@ucb.com>
2020-02-25 20:44:54 +0100

    addition

--- vocab.py
+++ vocab.py
@@ -158,9 +158,9 @@ class VocabEntry(object):
         ###     previous parts
         char_ids = self.words2charindices(sents)
         sents_t = pad_sents_char(char_ids,self.char2id['<pad>'] )
-        sents_var = torch.tensor(sents_t, device=device)
+        sents_var = torch.tensor(sents_t, dtype=torch.long, device=device)
         sents_var = sents_var.permute(1,0,2)
-        return torch.t(sents_var)
+        return sents_var
 
         ### END YOUR CODE
 
